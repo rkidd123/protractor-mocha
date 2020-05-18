adb75d2f7149e11bf7ee6f20c9e02c859b1805e1
Randall Kidd <randall.kidd@gmail.com>
2020-05-18 00:42:42 -0400

    wip

--- conf.js
+++ conf.js
@@ -25,7 +25,7 @@ exports.config = {
     capabilities: {
         browserName: 'chrome',
         shardTestFiles: true,
-        maxInstances: 1,
+        maxInstances: 4,
         chromeOptions: {
             args: [
                 // disable chrome's wakiness
